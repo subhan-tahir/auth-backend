83726882e2f5b052740ad9bf3bb67ec6a4c16905
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2025-01-14 15:28:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,9 @@ app.post('/employees', async (req, res) => {
 
 //login
 app.post('/login', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     // Extract user credentials from request body
     const { email, password } = req.body;
     console.log({ email, password });
