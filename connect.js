19934f2eb93244e74b52b9fc0e496d5ae79bc46f
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2025-01-14 16:42:18 +0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 let dbReport = 'test';
-const connectDB = ()=>  {
+const connectDB = async ()=>  {
   try {
     mongoose.set("strictQuery", true);
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
