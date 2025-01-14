62aee11f08bdcb9d5036258a13b71c690959c1c7
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2025-01-14 15:53:40 +0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const connectDB = ()=>{
 mongoose.connect(process.env.MONGO_URI)
-.then((res)=>console.log(res,'MongoDB connect!'))
-.catch((error)=>console.log(error,'MongoDB failed'))
+.then((res)=>{
+  console.log(res,'MongoDB connect!');
+  return 'Connected Mongo'
+})
+.catch((error)=>{
+  console.log(error,'MongoDB failed');
+  return 'Not connected Mongo'
+})
 }
 module.exports = connectDB;
