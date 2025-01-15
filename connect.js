54aded324f5644a21ae0354eb96bfa9d712a8ee2
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2025-01-15 13:30:53 +0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,37 +1,8 @@
 const mongoose = require('mongoose');
-// let dbReport = 'test';
-const connectDB = async ()=>  {
-  try {
-    mongoose.set("strictQuery", true);
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Database Connected Successfully");
-    // dbReport = "Database Connected Successfully";
-  } catch (error) {
-    console.log("DAtabase error");
-    // dbReport = "Database Connection Failed";
-  }
-};
-
-// const getDbReport = () => dbReport;
-
-// module.exports = {
-//   connectDB,
-//   getDbReport,
-// }
-
-//   {
-// mongoose.connect(process.env.MONGO_URI)
-// .then((res)=>{
-//   console.log(res,'MongoDB connect!');
-//   return 'Connected Mongo'
-// })
-// .catch((error)=>{
-//   console.log(error,'MongoDB failed');
-//   return 'Not connected Mongo'
-// })
-// }
+const connectDB = ()=>{
+mongoose.connect(process.env.MONGO_URI)
+.then((res)=>console.log('MongoDB connect!'))
+.catch((error)=>console.log(error,'MongoDB failed'))
+}
 module.exports = connectDB;
 // module.exports = dbReport;
