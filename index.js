d4a4fb4bff84bf31f3d3ab6d58fa6b870ac75ef8
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2025-01-22 15:21:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 //Production
 app.use(cors({
-    origin: ['https://auth-frontened.vercel.app/'] ,
+    origin: ['https://auth-frontened.vercel.app'] ,
     methods: ['GET', 'POST'], 
     credentials: true,
 }));
